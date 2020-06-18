f8e077c2194e5d17cd83777e14ab8ff99fed9332
puzzleleaf <cmtyx2@naver.com>
2020-06-19 02:38:17 +0900

    add repository query bloc

---
+++ lib/blocs/repository_query_bloc.dart
@@ -0,0 +1,41 @@
+import 'package:gitboard/models/repository_model.dart';
+import 'package:gitboard/models/repository_query_model.dart';
+import 'package:gitboard/resources/repository.dart';
+import 'package:rxdart/rxdart.dart';
+
+class RepositoryQueryBloc {
+  final _repository = Repository();
+
+  final _repositoryQuery = BehaviorSubject<RepositoryQueryModel>();
+
+  final _trendingRepositoriesFetcher =
+      BehaviorSubject<Future<List<RepositoryModel>>>();
+
+  Stream<Future<List<RepositoryModel>>> get trendingRepositories =>
+      _trendingRepositoriesFetcher.stream;
+
+  Stream<RepositoryQueryModel> get repositoryQuery => _repositoryQuery.stream;
+
+  RepositoryQueryBloc() {
+    _repositoryQuery.stream.transform(_queryTransformer()).pipe(_trendingRepositoriesFetcher);
+  }
+
+  void fetchRepositoryQuery(RepositoryQueryModel query) {
+    _repositoryQuery.sink.add(query);
+  }
+
+  _queryTransformer() {
+    return ScanStreamTransformer(
+        (Future<List<RepositoryModel>> trendingRepositories, RepositoryQueryModel query, int index) {
+          trendingRepositories = _repository.fetchTrendingRepositories(query.language.urlParam, query.since.toString().split('.').last, '');
+          return trendingRepositories;
+        },
+    );
+  }
+
+  dispose() async {
+    await _repositoryQuery.drain();
+    await _trendingRepositoriesFetcher.drain();
+    _trendingRepositoriesFetcher.close();
+  }
+}
