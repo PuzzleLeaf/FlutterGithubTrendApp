921f10d088db7a30aed3792f36c252dd70ba95dd
Puzzleleaf <cmtyx2@naver.com>
2020-06-20 22:34:10 +0900

    add brightness, portraitup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gitboard/models/repository_query_model.dart';
 import 'package:gitboard/screens/chart_screen.dart';
 import 'package:provider/provider.dart';
@@ -13,15 +14,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Gitboard',
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           elevation: 0,
+          brightness: Brightness.light,
           color: Colors.transparent,
           textTheme: TextTheme(
-            headline6:TextStyle(
+            headline6: TextStyle(
               color: Colors.black,
             )
           ),
